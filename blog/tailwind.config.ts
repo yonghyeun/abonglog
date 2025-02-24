bdccfb6d520e7c01790e68a06836c00393ea563b
yonghyeun <ttddcc119@naver.com>
2025-02-25 01:24:19 +0900

    feat[#1] : 배경색 tailwind config 추가

--- blog/tailwind.config.ts
+++ blog/tailwind.config.ts
@@ -4,9 +4,14 @@ export default {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{tsx,ts}"],
   theme: {
     extend: {
+      backgroundColor: {
+        primary: "var(--bg-primary)",
+        secondary: "var(--bg-secondary)",
+        tertiary: "var(--bg-tertiary)"
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)"
+        "sky-blue": "var(--sky-blue)",
+        "bright-blue": "var(--bright-blue)"
       }
     }
   },
