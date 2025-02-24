60d67509a07f46117e6803f7b518f3a08bd81d16
yonghyeun <ttddcc119@naver.com>
2025-02-24 12:00:00 +0900

    chore[#1] : 스토리북 install 및 환경 설정

---
+++ blog/src/shared/ui/Button.stories.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./Button";
+
+const meta = {
+  title: "shared/Button",
+  component: Button,
+};
+
+export default meta;
+
+export const Default = {
+  render: () => <Button>hi</Button>,
+};
---
+++ blog/src/shared/ui/Button.tsx
@@ -0,0 +1,3 @@
+export const Button: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <button className="text-red-600">{children}</button>;
+};
