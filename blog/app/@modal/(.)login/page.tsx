35f3947029fa1c5187660ba3257b2e1641bdf3f7
yonghyeun <ttddcc119@naver.com>
2025-02-26 15:39:43 +0900

    feat[8] : RoutingModal 컴포넌트 생성
    - 서버 컴포넌트를 children 으로 받아 클라이언트 컴포넌트 로직과 서버 액션을 함께 사용 할 수 있도록 하게 하기 위함

--- blog/app/@modal/(.)login/page.tsx
+++ blog/app/@modal/(.)login/page.tsx
@@ -1,15 +1,12 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
 import { OAuthLoginForm } from "@/features/auth/ui";
 
+import { RoutingModal } from "@/shared/ui/RoutingModal";
+
 const LoginModal = () => {
-  const router = useRouter();
   return (
-    <section className="absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-30">
-      <OAuthLoginForm isModal onClose={() => router.back()} />
-    </section>
+    <RoutingModal>
+      <OAuthLoginForm />
+    </RoutingModal>
   );
 };
 
