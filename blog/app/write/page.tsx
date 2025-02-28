6007dc7b930fa308b1ccd0768085d44c8034d24b
yonghyeun <ttddcc119@naver.com>
2025-02-28 18:25:48 +0900

    feat[#11] : 글 쓰기 페이지 1차 퍼블리싱

--- blog/app/write/page.tsx
+++ blog/app/write/page.tsx
@@ -1,5 +1,41 @@
 const WritePage = () => {
-  return <div>write page</div>;
+  return (
+    <section className="flex h-screen rounded-md px-8">
+      {/* 글 작성 페이지 */}
+      <div className="h-full w-full p-2 md:w-1/2">
+        <form className="flex flex-col gap-2">
+          {/* title */}
+          <div>
+            <label htmlFor="" className="sr-only">
+              제목
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              className="p-2 text-5xl outline-none focus:outline-none"
+              placeholder="제목을 입력해주세요"
+            />
+            <div className="h-2 w-32 bg-secondary" />
+          </div>
+
+          {/* tags */}
+          <label className="sr-only" htmlFor="tags">
+            태그
+          </label>
+          <input
+            id="tags"
+            name="tags"
+            type="text"
+            className="w-full border p-2 outline-none focus:outline-none"
+            placeholder="태그를 추가해주세요"
+          />
+        </form>
+      </div>
+      {/* 글 미리보기 페이지 */}
+      <div className="hidden h-full w-1/2 bg-secondary p-2 md:block">1</div>
+    </section>
+  );
 };
 
 export default WritePage;
