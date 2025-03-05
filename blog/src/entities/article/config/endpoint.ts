187e06713200dd7ddaed541b9d75445794a7cb7c
yonghyeun <ttddcc119@naver.com>
2025-03-05 11:17:07 +0900

    refactor[#17] : 사용되지 않는 엔드포인트 제거

--- blog/src/entities/article/config/endpoint.ts
+++ blog/src/entities/article/config/endpoint.ts
@@ -1,6 +1,5 @@
 export const ARTICLE_ENDPOINT = {
   POST_ARTICLE_IMAGE: () => "/api/articles/images" as const,
   POST_ARTICLE_THUMBNAIL: () => "/api/articles/thumbnails" as const,
-  POST_NEW_ARTICLE: () => "/api/articles" as const,
-  POST_ARTICLE_TAGS: () => "/api/articles" as const
+  POST_NEW_ARTICLE: () => "/api/articles" as const
 } as const;
