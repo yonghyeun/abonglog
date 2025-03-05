9ff8e198c3cb2748b20be9389060b39274690f95
yonghyeun <ttddcc119@naver.com>
2025-03-05 10:25:54 +0900

    refactor[#17] : api 엔드포인트 restful 원칙에 맞춰 복수형으로 변경

--- blog/src/entities/article/config/endpoint.ts
+++ blog/src/entities/article/config/endpoint.ts
@@ -1,7 +1,6 @@
 export const ARTICLE_ENDPOINT = {
-  POST_ARTICLE_IMAGE: () => "/api/article/image" as const,
-  // TODO : Restfull API 를 위해 articleId 를 파라미터로 넣지 않도록 수정
-  POST_ARTICLE_THUMBNAIL: () => `/api/article/thumbnail` as const,
+  POST_ARTICLE_IMAGE: () => "/api/articles/image" as const,
+  POST_ARTICLE_THUMBNAIL: () => "/api/articles/thumbnail" as const,
   POST_NEW_ARTICLE: () => "/api/articles" as const,
   POST_ARTICLE_TAGS: () => "/api/articles" as const
 } as const;
