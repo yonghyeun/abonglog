16aa0c8fe68f810190280fc72e3688138c392da8
yonghyeun <ttddcc119@naver.com>
2025-03-05 17:44:36 +0900

    feat[#21] : SeriesItem date 표기 방식 변경

--- blog/src/entities/series/ui/SeriesItem.tsx
+++ blog/src/entities/series/ui/SeriesItem.tsx
@@ -12,13 +12,13 @@ export const SeriesItem: React.FC<SeriesItemProps> = ({
   updatedAt
 }) => {
   return (
-    <div className="flex-grow rounded-md border bg-primary p-2">
+    <div className="flex flex-grow flex-col gap-2 rounded-md border bg-primary p-4">
       <div className="flex items-center gap-2">
         <h3>{seriesName}</h3>
         <span className="text-sm text-gray-400">({numOfArticles})</span>
       </div>
       <p className="text-xs text-gray-500">
-        마지막 업데이트: <time>{updatedAt}</time>
+        마지막 업데이트: <time>{new Date(updatedAt).toLocaleString()}</time>
       </p>
     </div>
   );
