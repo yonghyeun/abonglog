0d004151529feb8c53a34b3777fb3f7546a18aeb
yonghyeun <ttddcc119@naver.com>
2025-03-05 11:16:08 +0900

    refactor[#17] : image -> images , thumbnail -> thumbnails 로 리소스명 변경

--- blog/src/entities/article/config/endpoint.ts
+++ blog/src/entities/article/config/endpoint.ts
@@ -1,6 +1,6 @@
 export const ARTICLE_ENDPOINT = {
-  POST_ARTICLE_IMAGE: () => "/api/articles/image" as const,
-  POST_ARTICLE_THUMBNAIL: () => "/api/articles/thumbnail" as const,
+  POST_ARTICLE_IMAGE: () => "/api/articles/images" as const,
+  POST_ARTICLE_THUMBNAIL: () => "/api/articles/thumbnails" as const,
   POST_NEW_ARTICLE: () => "/api/articles" as const,
   POST_ARTICLE_TAGS: () => "/api/articles" as const
 } as const;
