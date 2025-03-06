e457db195ddaa608163292365eee2ff6f570faa5
yonghyeun <ttddcc119@naver.com>
2025-03-06 23:03:32 +0900

    feat[#27] : Chip 공통 컴포넌트 생성

---
+++ blog/src/shared/ui/Chip.tsx
@@ -0,0 +1,45 @@
+const chipStyles = [
+  "bg-sky-100 text-sky-500 hover:bg-sky-200",
+  "bg-red-100 text-red-500 hover:bg-red-200",
+  "bg-green-100 text-green-500 hover:bg-green-200",
+  "bg-yellow-100 text-yellow-500 hover:bg-yellow-200",
+  "bg-blue-100 text-blue-500 hover:bg-blue-200",
+  "bg-indigo-100 text-indigo-500 hover:bg-indigo-200",
+  "bg-purple-100 text-purple-500 hover:bg-purple-200",
+  "bg-pink-100 text-pink-500 hover:bg-pink-200",
+  "bg-orange-100 text-orange-500 hover:bg-orange-200",
+  "bg-teal-100 text-teal-500 hover:bg-teal-200",
+  "bg-cyan-100 text-cyan-500 hover:bg-cyan-200",
+  "bg-lime-100 text-lime-500 hover:bg-lime-200",
+  "bg-amber-100 text-amber-500 hover:bg-amber-200",
+  "bg-rose-100 text-rose-500 hover:bg-rose-200",
+  "bg-fuchsia-100 text-fuchsia-500 hover:bg-fuchsia-200",
+  "bg-violet-100 text-violet-500 hover:bg-violet-200",
+  "bg-emerald-100 text-emerald-500 hover:bg-emerald-200",
+  "bg-amber-100 text-amber-500 hover:bg-amber-200",
+  "bg-lime-100 text-lime-500 hover:bg-lime-200",
+  "bg-teal-100 text-teal-500 hover:bg-teal-200"
+] as const;
+
+interface ChipProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  theme: number;
+}
+
+export const Chip: React.FC<ChipProps> = ({
+  children,
+  theme,
+  className = "",
+  ...props
+}) => {
+  return (
+    <button
+      className={`${
+        chipStyles[(theme % chipStyles.length) - 1]
+      } rounded-full px-4 py-1 text-xs ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
