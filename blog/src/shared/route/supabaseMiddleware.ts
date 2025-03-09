ce59be40c9e5dbb58439ed6b9cb02f397348bb7f
yonghyeun <ttddcc119@naver.com>
2025-03-09 12:20:01 +0900

    [Fix] middleware config 수정,  write 경로에 대해서만 동작하도록 수정

--- blog/src/shared/route/supabaseMiddleware.ts
+++ blog/src/shared/route/supabaseMiddleware.ts
@@ -7,35 +7,26 @@ export async function updateSession(request: NextRequest) {
     request
   });
 
+  /**
+   * referechToken 을 자동 갱신하는 옵션을 설정해두었기 때문에
+   * getUser 만 호출해도 자동으로 refreshToken 이 갱신된다.
+   *
+   * 만약 refreshToken 이 갱신되지 않는다면, refreshToken 이 만료되었을 가능성이 높다.
+   * (supabase 에선 refreshToken을 사용하고나면 해당 referehsToken을 무효화 시킨다.
+   * 실제 사용하며 refrehToken 이 만료되었을 경우는 두 개의 브라우저에서 동일한 refreshToken 을 들고 있다가
+   * 다른 브라우저에서 해당 refreshToken을 사용한 경우일 것이다.
+   * )
+   */
   const supabase = await createServerSupabase();
-
   const {
     data: { user }
   } = await supabase.auth.getUser();
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
+  if (!user) {
     const url = request.nextUrl.clone();
-    url.pathname = "/login";
+    url.pathname = "/auth";
     return NextResponse.redirect(url);
   }
-
-  // IMPORTANT: You *must* return the supabaseResponse object as it is.
-  // If you're creating a new response object with NextResponse.next() make sure to:
-  // 1. Pass the request in it, like so:
-  //    const myNewResponse = NextResponse.next({ request })
-  // 2. Copy over the cookies, like so:
-  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-  // 3. Change the myNewResponse object to fit your needs, but avoid changing
-  //    the cookies!
-  // 4. Finally:
-  //    return myNewResponse
-  // If this is not done, you may be causing the browser and server to go out
-  // of sync and terminate the user's session prematurely!
 
   return supabaseResponse;
 }
