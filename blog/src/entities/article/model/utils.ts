19ae0083d8a750435a751bc8a9142317251bb9a9
yonghyeun <ttddcc119@naver.com>
2025-03-12 01:16:44 +0900

    refactor[#66] : extractTagName 타입가드 추가하여 컴파일러에게 정보 제공

--- blog/src/entities/article/model/utils.ts
+++ blog/src/entities/article/model/utils.ts
@@ -2,7 +2,7 @@ import { parsingHeading, rehypeMarkdown } from "../lib";
 
 export const extractTagName = (articleTags: { tagName: string | null }[]) => {
   return articleTags
-    .filter(({ tagName }) => !tagName)
+    .filter((tag): tag is { tagName: string } => tag.tagName !== null)
     .map(({ tagName }) => tagName);
 };
 
