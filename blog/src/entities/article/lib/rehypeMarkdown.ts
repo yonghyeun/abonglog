fea361107228b52a40a7fe0533b63d5bece73400
yonghyeun <ttddcc119@naver.com>
2025-03-11 23:28:03 +0900

    feat[#61] : 코드블록 css 설정
    - attr() 속성을 통해 사용한 언어 나타내기
    - 기존 data-language 속성 변경되지 않던 버그 픽스

--- blog/src/entities/article/lib/rehypeMarkdown.ts
+++ blog/src/entities/article/lib/rehypeMarkdown.ts
@@ -18,6 +18,16 @@ export const rehypeMarkdown = async (markdown: string) => {
     // HTML로 변환된 AST를 문자열로 변환 합니다.
     .use(rehypeSlug)
     // 각 Heading 태그에 id 를 추가 합니다.
+    // 코드 블럭을 예쁘게 표현하기 위한 설정을 추가 합니다.
+    .use(rehypePrettyCode, {
+      grid: true,
+      theme: "tokyo-night",
+      keepBackground: false,
+      bypassInlineCode: false,
+      defaultLang: {
+        block: "typescript"
+      }
+    })
     .use(rehypeAddClasses, {
       h1: "heading-1",
       h2: "heading-2",
@@ -45,16 +55,6 @@ export const rehypeMarkdown = async (markdown: string) => {
       del: "strikethrough"
     })
     .use(rehypeStringfy)
-    // 코드 블럭을 예쁘게 표현하기 위한 설정을 추가 합니다.
-    .use(rehypePrettyCode, {
-      grid: true,
-      theme: "tokyo-night",
-      keepBackground: false,
-      bypassInlineCode: false,
-      defaultLang: {
-        block: "typescript"
-      }
-    })
     .process(markdown);
 
   return vfileObject.toString();
