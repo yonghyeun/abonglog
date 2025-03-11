3bec91f38dd95946560bcc43dc3fd4c343d99537
yonghyeun <ttddcc119@naver.com>
2025-03-11 17:48:31 +0900

    refactor[#59] : content 를 html , headings 로 데이터 만들던 과정 컴포넌트 내부가 아닌 메소드 내부에서 처리

--- blog/app/article/[articleId]/page.tsx
+++ blog/app/article/[articleId]/page.tsx
@@ -1,8 +1,5 @@
 import { AdminArticlePage, GuestArticlePage } from "@/views/[article]/ui";
 
-import { parsingHeading } from "@/features/article/lib/createNestedHeadings";
-
-import { rehypeMarkdown } from "@/entities/article/lib";
 import { getArticleById } from "@/entities/article/model";
 import { getAuthorizedUser } from "@/entities/user/model";
 
@@ -14,8 +11,6 @@ const ArticlePage: React.FC<ArticlePageProps> = async ({ params }) => {
   const { articleId } = await params;
   const articleData = await getArticleById(articleId);
 
-  const html = await rehypeMarkdown(articleData.content);
-  const headings = parsingHeading(articleData.content);
   const {
     data: { user }
   } = await getAuthorizedUser();
@@ -25,9 +20,7 @@ const ArticlePage: React.FC<ArticlePageProps> = async ({ params }) => {
       <AdminArticlePage
         articleId={articleId}
         articleData={{
-          ...articleData,
-          headings,
-          html
+          ...articleData
         }}
       />
     );
@@ -37,9 +30,7 @@ const ArticlePage: React.FC<ArticlePageProps> = async ({ params }) => {
     <GuestArticlePage
       articleId={articleId}
       articleData={{
-        ...articleData,
-        headings,
-        html
+        ...articleData
       }}
     />
   );
