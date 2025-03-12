133a72f80c8b1a8c17511c91b55ca4a5bd1d3161
yonghyeun <ttddcc119@naver.com>
2025-03-12 22:47:44 +0900

    feat[#73] : list/[series] 경로 generateStaticParams 적용
    - dynamicParams 옵션 제거

--- blog/app/list/[series]/page.tsx
+++ blog/app/list/[series]/page.tsx
@@ -3,14 +3,13 @@ import {
   EveryArticleListSlot
 } from "@/slots/article/ui";
 import { HydrationBoundary } from "@tanstack/react-query";
-import Link from "next/link";
 
 import {
-  getArticleInfoPerSeries,
   getArticleList,
   getArticleListBySeries,
   getNumberOfArticles
 } from "@/entities/article/model";
+import { getSeriesList } from "@/entities/series/model";
 
 import {
   prefetchInfiniteQueryInServer,
@@ -23,11 +22,15 @@ interface ArticleListPageProps {
   }>;
 }
 
+export async function generateStaticParams() {
+  const data = await getSeriesList().queryFn();
+  return [{ series: "all" }, ...data.map(({ name }) => ({ series: name }))];
+}
+
+export const dynamicParams = false;
+
 const ArticleListPage: React.FC<ArticleListPageProps> = async ({ params }) => {
   const series = decodeURIComponent((await params).series || "all");
-
-  // TODO 에러바운더리 도입하면 해당 쿼리문 제거하기
-  const numOfSeriesArray = await getArticleInfoPerSeries().queryFn();
 
   const numOfArticlesState = await prefetchQueryInServer(() =>
     getNumberOfArticles(series)
@@ -53,53 +56,47 @@ const ArticleListPage: React.FC<ArticleListPageProps> = async ({ params }) => {
     );
   }
 
-  const searchedSeries = numOfSeriesArray.find(
-    (data) => data.seriesName === series
+  // 시리즈 별로 모아보기인 경우
+  const articleListState = await prefetchInfiniteQueryInServer(() =>
+    getArticleListBySeries("published", series)
   );
 
-  // 시리즈 별로 모아보기인 경우
-  if (searchedSeries) {
-    const articleListState = await prefetchInfiniteQueryInServer(() =>
-      getArticleListBySeries("published", series)
-    );
-
-    return (
-      <HydrationBoundary
-        state={{
-          ...articleListState,
-          ...numOfArticlesState
-        }}
-      >
-        <section className="media-padding-x flex min-h-screen flex-col">
-          <ArticleListBySeriesSlot seriesName={series} />
-        </section>
-      </HydrationBoundary>
-    );
-  }
+  return (
+    <HydrationBoundary
+      state={{
+        ...articleListState,
+        ...numOfArticlesState
+      }}
+    >
+      <section className="media-padding-x flex min-h-screen flex-col">
+        <ArticleListBySeriesSlot seriesName={series} />
+      </section>
+    </HydrationBoundary>
+  );
 
   // 찾는 시리즈가 없는 경우 404
   // TODO errorboundary 를 통해 404 페이지로 이동시키기
-  return <NotFound series={series} />;
+  // return <NotFound series={series} />;
 };
 
-const NotFound: React.FC<{ series: string }> = ({ series }) => (
-  <section className="media-padding-x flex min-h-screen flex-col">
-    <section className="my-12 flex items-center justify-center">
-      <p className="text-[10rem] text-gray-400">Σ(°ロ°)</p>
-    </section>
-    <div className="flex flex-col items-center justify-center gap-4">
-      <h1>
-        아쉽게도 입력하신 <span className="text-blue-700">{series}</span>는
-        존재하지 않는 시리즈 이름입니다.
-      </h1>
-      <Link
-        href="/"
-        className="border border-blue-700 bg-blue-700 px-8 py-4 text-white"
-      >
-        메인 페이지로 돌아가기
-      </Link>
-    </div>
-  </section>
-);
+// const NotFound: React.FC<{ series: string }> = ({ series }) => (
+//   <section className="media-padding-x flex min-h-screen flex-col">
+//     <section className="my-12 flex items-center justify-center">
+//       <p className="text-[10rem] text-gray-400">Σ(°ロ°)</p>
+//     </section>
+//     <div className="flex flex-col items-center justify-center gap-4">
+//       <h1>
+//         아쉽게도 입력하신 <span className="text-blue-700">{series}</span>는
+//         존재하지 않는 시리즈 이름입니다.
+//       </h1>
+//       <Link
+//         href="/"
+//         className="border border-blue-700 bg-blue-700 px-8 py-4 text-white"
+//       >
+//         메인 페이지로 돌아가기
+//       </Link>
+//     </div>
+//   </section>
+// );
 
 export default ArticleListPage;
