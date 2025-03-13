695637a514cdbba73749bc2ce9c524c501426e47
yonghyeun <ttddcc119@naver.com>
2025-03-13 16:49:57 +0900

    feat[#79] : 디자인 토큰으로 사용 할 배경색과 글자색 생성

--- blog/tailwind.config.ts
+++ blog/tailwind.config.ts
@@ -1,12 +1,25 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class", // 다크 모드 활성화
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{tsx,ts}"],
   theme: {
     extend: {
       backgroundColor: {
         primary: "var(--bg-primary)",
-        secondary: "var(--bg-secondary)"
+        secondary: "var(--bg-secondary)",
+        darkPrimary: "var(--bg-dark-primary)",
+        darkSecondary: "var(--bg-dark-secondary)"
+      },
+      textColor: {
+        primary: "var(--text-primary)",
+        secondary: "var(--text-secondary)",
+        tertiary: "var(--text-tertiary)",
+        quaternary: "var(--text-quaternary)",
+        darkPrimary: "var(--text-dark-primary)",
+        darkSecondary: "var(--text-dark-secondary)",
+        darkTertiary: "var(--text-dark-tertiary)",
+        darkQuaternary: "var(--text-dark-quaternary)"
       }
     }
   },
