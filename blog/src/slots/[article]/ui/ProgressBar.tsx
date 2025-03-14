434828fabe3d2c20adb7651a6e046f245db29ec4
yonghyeun <ttddcc119@naver.com>
2025-03-14 20:24:23 +0900

    feat[#88] : ProgressBar 에서 0으로 나누는 오류 방지하기 위해 삼항 연산자 추가

--- blog/src/slots/[article]/ui/ProgressBar.tsx
+++ blog/src/slots/[article]/ui/ProgressBar.tsx
@@ -6,16 +6,16 @@ export const ProgressBar = () => {
   const [progress, setProgress] = useState<number>(0);
 
   useEffect(() => {
-    const handleProgress = () => {
+    const handleScrollChange = () => {
       const totalHeight = document.body.scrollHeight - window.innerHeight;
-      const progress = window.scrollY / totalHeight;
+      const progress = totalHeight > 0 ? window.scrollY / totalHeight : 0;
       setProgress(progress);
     };
 
-    window.addEventListener("scroll", handleProgress);
+    window.addEventListener("scroll", handleScrollChange);
 
     return () => {
-      window.removeEventListener("scroll", handleProgress);
+      window.removeEventListener("scroll", handleScrollChange);
     };
   }, []);
 
