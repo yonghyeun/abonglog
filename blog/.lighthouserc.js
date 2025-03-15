d682e7b75f93394ec296c5a4a0f20626058eb521
yonghyeun <ttddcc119@naver.com>
2025-03-15 20:36:40 +0900

    fix[#90] : serverReadyTimeout 생성
    - 로컬 환경에선 곧바로 실행 됐지만 CI 환경에선 타임아웃이 필요한듯 하다

--- blog/.lighthouserc.js
+++ blog/.lighthouserc.js
@@ -1,6 +1,10 @@
 module.exports = {
   ci: {
     collect: {
+      // 서버 준비 대기 시간 증가
+      startServerReadyTimeout: 60000,
+      // Next.js 서버 준비 메시지 패턴
+      startServerReadyPattern: "- ready started server on",
       startServerCommand: "npm run start",
       url: ["http://localhost:3000"]
     },
