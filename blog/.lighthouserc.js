616080d30d1e34cdeccdc720da699070d085ec4f
yonghyeun <ttddcc119@naver.com>
2025-03-16 09:49:05 +0900

    fix[#90] : lighthouse 에서 TLS error 던지지 않도록 수정

--- blog/.lighthouserc.js
+++ blog/.lighthouserc.js
@@ -7,11 +7,30 @@ module.exports = {
       startServerReadyTimeout: 60000,
       numberOfRuns: 3
     },
+
+    // TLS 인증서 오류를 무시하기 위한 Chrome 플래그 설정
+
+    chromeFlags: [
+      "--no-sandbox",
+      "--ignore-certificate-errors",
+      "--allow-insecure-localhost",
+      "--disable-gpu"
+    ],
+
+    // TLS 인증서 오류를 무시하기 위한 Lighthouse 설정
+
+    settings: {
+      skipAudits: [
+        "is-on-https",
+        "redirects-http",
+        "uses-http2",
+        "uses-long-cache-ttl"
+      ]
+    },
     upload: {
       target: "filesystem",
       outputDir: "lhci_reports",
-      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
-      startServerCommand: "npm run start"
+      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%"
     }
   }
 };
