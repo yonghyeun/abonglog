dfabb6d69e6d0764f05a269c6776b2962d6576cc
yonghyeun <ttddcc119@naver.com>
2025-03-16 17:39:52 +0900

    fix[93] : ArticleEditPage hydrationState , Suspesne block 추가

--- blog/app/write/[articleId]/page.tsx
+++ blog/app/write/[articleId]/page.tsx
@@ -1,6 +1,12 @@
 import { ArticleWriteSlot } from "@/slots/write/ui";
+import { HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 import { getArticleById } from "@/entities/article/model";
+import { getSeriesList } from "@/entities/series/model";
+import { getTagList } from "@/entities/tag/model";
+
+import { prefetchQueryInServer } from "@/shared/model";
 
 interface ArticleEditPageProps {
   params: Promise<{
@@ -8,18 +14,28 @@ interface ArticleEditPageProps {
   }>;
 }
 
+const getArticleEditState = (articleId: string) => {
+  return Promise.all([
+    getArticleById(articleId, null),
+    prefetchQueryInServer(getTagList, getSeriesList)
+  ]);
+};
+
 const ArticleEditPage: React.FC<ArticleEditPageProps> = async ({ params }) => {
   const { articleId } = await params;
-
-  const articleData = await getArticleById(articleId, null);
+  const [articleData, articleWriteState] = await getArticleEditState(articleId);
 
   return (
-    <ArticleWriteSlot
-      articleId={+articleId}
-      initialState={{
-        ...articleData
-      }}
-    />
+    <HydrationBoundary state={articleWriteState}>
+      <Suspense>
+        <ArticleWriteSlot
+          articleId={+articleId}
+          initialState={{
+            ...articleData
+          }}
+        />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
