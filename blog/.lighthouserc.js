2dfd0ba1ccb175bfc09f1e3ef813b35aac5f9df5
yonghyeun <ttddcc119@naver.com>
2025-03-16 09:53:59 +0900

    fix[#90] : 잘못된 bracket 에 있는 setting 구문 수정

--- blog/.lighthouserc.js
+++ blog/.lighthouserc.js
@@ -5,28 +5,29 @@ module.exports = {
       startServerCommand: "npm run start",
       startServerReadyPattern: "ready on",
       startServerReadyTimeout: 60000,
-      numberOfRuns: 3
+      numberOfRuns: 3,
+
+      // TLS 인증서 오류를 무시하기 위한 Chrome 플래그 설정
+
+      chromeFlags: [
+        "--no-sandbox",
+        "--ignore-certificate-errors",
+        "--allow-insecure-localhost",
+        "--disable-gpu"
+      ],
+
+      // TLS 인증서 오류를 무시하기 위한 Lighthouse 설정
+
+      settings: {
+        skipAudits: [
+          "is-on-https",
+          "redirects-http",
+          "uses-http2",
+          "uses-long-cache-ttl"
+        ]
+      }
     },
 
-    // TLS 인증서 오류를 무시하기 위한 Chrome 플래그 설정
-
-    chromeFlags: [
-      "--no-sandbox",
-      "--ignore-certificate-errors",
-      "--allow-insecure-localhost",
-      "--disable-gpu"
-    ],
-
-    // TLS 인증서 오류를 무시하기 위한 Lighthouse 설정
-
-    settings: {
-      skipAudits: [
-        "is-on-https",
-        "redirects-http",
-        "uses-http2",
-        "uses-long-cache-ttl"
-      ]
-    },
     upload: {
       target: "filesystem",
       outputDir: "lhci_reports",
