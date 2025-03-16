c0fb162efd2e657c5d84c9c65d39d91e574018fa
yonghyeun <ttddcc119@naver.com>
2025-03-16 09:36:36 +0900

    fix[#90] : startServerReadyPattern 추가

--- blog/.lighthouserc.js
+++ blog/.lighthouserc.js
@@ -3,6 +3,7 @@ module.exports = {
     collect: {
       url: ["http://localhost:3000/"],
       startServerCommand: "npm run start",
+      startServerReadyPattern: "ready on",
       startServerReadyTimeout: 60000,
       numberOfRuns: 3
     },
