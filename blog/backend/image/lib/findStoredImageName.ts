44a6633fc6b2b494bfe6d796f7117dab315150df
yonghyeun <ttddcc119@naver.com>
2025-03-24 16:44:22 +0900

    fix[#119] : 이미지 경로 변경에 따른 findStoredImageName 메소드 로직 수정

--- blog/backend/image/lib/findStoredImageName.ts
+++ blog/backend/image/lib/findStoredImageName.ts
@@ -1,11 +1,13 @@
 import { findImageUrl } from "@/features/article/lib";
 
+import { SUPABASE_STORAGE_URL } from "@/shared/config";
+
 type FindStoredImageName = (content: string) => string[];
 
 export const findStoredImageName: FindStoredImageName = (content) => {
   return findImageUrl(content)
     .map(({ src }) => src)
-    .filter((url) => url.startsWith("/api/"))
+    .filter((url) => url.startsWith(SUPABASE_STORAGE_URL))
     .map((url) => url.split("/").pop())
     .filter((fileName) => fileName !== undefined) as string[];
 };
