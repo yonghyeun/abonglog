9211d08a3fff65368318279dd9ca40f62ced3c83
yonghyeun <ttddcc119@naver.com>
2025-03-29 20:55:52 +0900

    refactor[#133] : 오타 수정

--- blog/src/entities/series/ui/SeriesItem.tsx
+++ blog/src/entities/series/ui/SeriesItem.tsx
@@ -12,7 +12,7 @@ export const SeriesItem: React.FC<SeriesItemProps> = ({
   updatedAt
 }) => {
   return (
-    <div className="transtiion-all flex flex-grow transform flex-col gap-2 rounded-lg border bg-primary p-4 transition-transform duration-200 hover:scale-105">
+    <div className="transtion-all flex flex-grow transform flex-col gap-2 rounded-lg border bg-primary p-4 transition-transform duration-200 hover:scale-105">
       <div className="flex items-center gap-2">
         <h3 className="font-semibold text-primary">{seriesName}</h3>
         <span className="text-sm text-tertiary">({numOfArticles})</span>
