6e292c74719cbc48fd68aa49afa8d5e0e69989fe
yonghyeun <ttddcc119@naver.com>
2025-03-30 16:58:50 +0900

    feat[#135] : 낮은 색상을 가지는 tertiary 폰트 색상 사용하던 컴포넌트 제거

--- blog/src/entities/series/ui/SeriesItem.tsx
+++ blog/src/entities/series/ui/SeriesItem.tsx
@@ -12,12 +12,12 @@ export const SeriesItem: React.FC<SeriesItemProps> = ({
   updatedAt
 }) => {
   return (
-    <div className="transtion-all flex flex-grow transform flex-col gap-2 rounded-lg bg-primary p-4 transition-transform duration-200 hover:scale-105">
+    <div className="transtion-all flex flex-grow transform flex-col gap-2 rounded-lg bg-primary p-4 text-secondary transition-transform duration-200 hover:scale-105">
       <div className="flex items-center gap-2">
         <h3 className="font-semibold text-primary">{seriesName}</h3>
-        <span className="text-sm text-tertiary">({numOfArticles})</span>
+        <span className="text-sm">({numOfArticles})</span>
       </div>
-      <p className="text-xs text-secondary">
+      <p className="text-xs">
         마지막 업데이트: <time>{new Date(updatedAt).toLocaleString()}</time>
       </p>
     </div>
