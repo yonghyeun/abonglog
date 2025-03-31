d73342fa8f91c4ffc49764304b0ccb0123affd80
yonghyeun <ttddcc119@naver.com>
2025-03-31 16:30:00 +0900

    refactor[#138] : capitalizeFirstLetter refactoring

--- blog/src/entities/tag/lib/capitalizeFirstLetter.ts
+++ blog/src/entities/tag/lib/capitalizeFirstLetter.ts
@@ -1,3 +1,17 @@
-export const capitalizeFirstLetter = (str: string) => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+import { when } from "@fxts/core";
+
+type FirstLetterCapitalize<T extends string> = T["length"] extends 0
+  ? T
+  : T extends `${infer First}${infer Rest}`
+    ? `${Uppercase<First>}${Rest}`
+    : never;
+
+export const capitalizeFirstLetter = <T extends string>(
+  str: T
+): FirstLetterCapitalize<T> => {
+  return when(
+    (str) => str.length > 0,
+    () => str[0].toUpperCase() + str.slice(1),
+    str
+  ) as FirstLetterCapitalize<T>;
 };
