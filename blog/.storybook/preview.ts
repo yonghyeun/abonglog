6e709998530d77ff795b968e4c58dd6f8439ab89
yonghyeun <ttddcc119@naver.com>
2025-04-05 00:38:26 +0900

    feat[#145] : 스토리북 font 설정

--- blog/.storybook/preview.ts
+++ blog/.storybook/preview.ts
@@ -1,6 +1,25 @@
 import "../app/globals.css";
 import type { Preview } from "@storybook/react";
 
+const fontStyles = `
+  @import url('https://fonts.googleapis.com/css2?family=Gothic+A1:wght@400;700&display=swap');
+  
+  body {
+    font-family: 'Gothic A1', sans-serif;
+  }
+`;
+
+const injectFontStyles = () => {
+  if (typeof document !== "undefined") {
+    const styleElement = document.createElement("style");
+    styleElement.innerHTML = fontStyles;
+    document.head.appendChild(styleElement);
+  }
+};
+
+// 스타일 주입
+injectFontStyles();
+
 const preview: Preview = {
   tags: ["autodocs"],
   parameters: {
