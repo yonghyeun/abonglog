8cc3b205e205cae6a91c87a86415a363dde3e0f3
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2025-05-06 22:19:18 +0900

    [Feat] 이전글, 다음글 가져오는 부분 클라이언트단으로 이동 (#158)
    * feat[#157] : 이전글, 다음글 가져오는 부분 서버에서 페칭해오지 않도록 수정
    
    - SSG 로 인해 빌드타임 때 페칭 된 데이터로 footer 가 렌더됨
    
    * feat[#157] : 이전글과 현재글은 클라이언트단에서 매번 fresh 하게 fetching 해오도록 수정

---
+++ blog/src/slots/[article]/ui/ArticleFooter.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import {
+  BiSolidLeftArrowCircle,
+  BiSolidRightArrowCircle
+} from "react-icons/bi";
+
+import { useGetArticleGraph } from "@/entities/article/model/getArticleGraph";
+
+interface ArticleFooterProps {
+  articleId: string;
+  seriesName: string;
+}
+
+export const ArticleFooter: React.FC<ArticleFooterProps> = ({
+  articleId,
+  seriesName
+}) => {
+  const {
+    data: { prevArticleData, nextArticleData }
+  } = useGetArticleGraph(Number(articleId), seriesName);
+
+  return (
+    <footer className="flex flex-col justify-between gap-4 text-lg sm:flex-row sm:justify-between">
+      {/* 하단 네비게이션 버튼 */}
+      <div className="flex-1">
+        {prevArticleData && (
+          <Link
+            href={`/article/${prevArticleData.id}`}
+            className="flex h-full flex-1 items-center justify-start gap-2 rounded-lg border border-purple-700 p-2 text-purple-700 hover:text-purple-500 dark:border-purple-300 dark:text-purple-300 hover:dark:text-purple-200"
+          >
+            <BiSolidLeftArrowCircle />
+            {prevArticleData.title}
+          </Link>
+        )}
+      </div>
+      <div className="flex flex-1">
+        {nextArticleData && (
+          <Link
+            href={`/article/${nextArticleData.id}`}
+            className="flex h-full flex-1 items-center justify-end gap-2 rounded-lg border border-purple-700 p-2 text-purple-700 hover:text-purple-500 dark:border-purple-300 dark:text-purple-300 hover:dark:text-purple-200"
+          >
+            {nextArticleData.title}
+            <BiSolidRightArrowCircle />
+          </Link>
+        )}
+      </div>
+    </footer>
+  );
+};
