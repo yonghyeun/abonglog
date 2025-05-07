d252c689d0f25432d73929c2f1b9d492dfce5f52
yonghyeun <ttddcc119@naver.com>
2025-05-07 18:32:08 +0900

    refactor[#159] : 사용하지 않는 엔드포인트 변수명 제거

--- blog/src/entities/article/config/endpoint.ts
+++ blog/src/entities/article/config/endpoint.ts
@@ -1,7 +1,6 @@
 export const ARTICLE_ENDPOINT = {
   POST_ARTICLE_IMAGE: () => "/api/articles/images" as const,
   POST_ARTICLE_THUMBNAIL: () => "/api/articles/thumbnails" as const,
-  POST_NEW_ARTICLE: () => "/api/articles" as const, // TODO 제거하기
   UPSERT_ARTICLE: () => "/api/articles" as const,
   DELETE_ARTICLE: () => "/api/articles" as const
 } as const;
