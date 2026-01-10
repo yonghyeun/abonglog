2a6974df9b0741da587b09a23f67385e9a8cf6c5
yonghyeun <ttddcc119@naver.com>
2026-01-10 20:17:53 +0900

    refactor : 메인 레이아웃 및 Footer 조합
    layout.tsx에 Footer 컴포넌트 적용 및 메인 콘텐츠 영역 패딩 조정

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -8,10 +8,10 @@ import localFont from "next/font/local";
 import { Suspense } from "react";
 
 import { DarkModeInitializeScript } from "@/features/utils/ui";
+import { Footer } from "@/widgets/navigate/ui";
 
 import { getArticleMetaListPerSeries } from "@/entities/article/model";
 
-import { GithubIcon, HumanIcon } from "@/shared/config";
 import { prefetchQueryInServer } from "@/shared/model";
 
 const pretendard = localFont({
@@ -49,49 +49,18 @@ const RootLayout: React.FC<RootLayoutProps> = async ({ children, modal }) => {
           <HydrationBoundary state={sidebarState}>
             <Suspense fallback={null}>
               <Header />
-              <main className="flex flex-grow flex-col">
+              <main className="flex flex-grow flex-col pt-4 sm:pt-6 md:pt-8">
                 {modal}
                 {children}
                 <Analytics />
               </main>
+              <Footer />
             </Suspense>
           </HydrationBoundary>
-          <Footer />
         </ServiceProvider>
       </body>
     </html>
   );
 };
-
-const Footer = () => (
-  <footer className="media-padding-x py-4 text-secondary">
-    {/* introduce */}
-    <div className="flex flex-col justify-between gap-4 md:flex-row">
-      <div className="flex-grow">
-        <h3 className="mb-2">abonglog</h3>
-        <p className="mb-2">
-          공부한 내용을 기록하고 함께 성장하고 싶어 만든 두 번째 블로그입니다
-        </p>
-        <p className="mb-2">주로 웹개발과 관련된 내용을 포스팅합니다.</p>
-      </div>
-
-      <div className="flex flex-col justify-end">
-        <a
-          href="https://github.com/yonghyeun"
-          className="flex items-center gap-2 text-sm"
-        >
-          <GithubIcon />
-          Github
-        </a>
-        <span className="flex items-center gap-2 text-sm">
-          <HumanIcon /> ttddcc119@naver.com
-        </span>
-      </div>
-    </div>
-    <p className="mt-2 text-center italic">
-      @ 2025 abonglog All rights reserved
-    </p>
-  </footer>
-);
 
 export default RootLayout;
