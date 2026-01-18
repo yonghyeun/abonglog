5d6f28f862959c6068033cad82692b9cd34bec17
yonghyeun <ttddcc119@naver.com>
2026-01-18 11:27:10 +0900

    [Hotfix] Jest 테스트 코드 스캔 범위 수정

--- blog/jest.config.js
+++ blog/jest.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  maxWorkers: "50%",
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/coverage/",
+    "<rootDir>/docs/",
+    "<rootDir>/public/"
+  ],
+  modulePathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/coverage/",
+    "<rootDir>/docs/",
+    "<rootDir>/public/"
+  ],
+  watchPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/coverage/",
+    "<rootDir>/docs/",
+    "<rootDir>/public/"
+  ],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "^@backend/(.*)$": "<rootDir>/backend/$1",
